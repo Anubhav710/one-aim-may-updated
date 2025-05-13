de9cac33f7bac93941c255d1d22da94999a19268
Anubhav Agrawal <anubhav@UTXOTechs-MacBook-Air.local>
2025-05-13 09:56:40 +0530

    updated website

--- src/app/(root)/test-series/components/HowItWork.tsx
+++ src/app/(root)/test-series/components/HowItWork.tsx
@@ -9,51 +9,129 @@ interface StepItemProps {
   isReversed?: boolean;
 }
 
-const StepItem = ({ number, title, isReversed = false }: StepItemProps) => {
-  return (
-    <div
-      className={`flex flex-col md:flex-row items-center gap-4  md:gap-8 mb-8 md:mb-12 ${
-        isReversed ? "md:flex-row-reverse" : ""
-      }`}
-    >
-      <div className="flex flex-col relative w-full  md:w-auto order-2 md:order-none">
-        <div className="relative mb-3">
-          <h1 className="bg-white w-max py-2 px-4 text-lg font-medium shadow-sm max-sm:text-sm">
-            {title}
-          </h1>
-          {/* <div className="w-0 h-0 border-l-[10px] border-l-transparent border-t-[15px] border-t-white border-r-[10px] border-r-transparent absolute bottom-[-15px] left-0 transform"></div> */}
-        </div>
-      </div>
-      <span className="bg-gradient-to-t text-7xl md:text-8xl font-bold from-[#ffffff] to-[#FF7B07] text-transparent bg-clip-text order-1 md:order-none">
-        {number}
-      </span>
-    </div>
-  );
-};
+// const StepItem = ({ number, title, isReversed = false }: StepItemProps) => {
+//   return (
+//     <div
+//       className={`flex flex-col md:flex-row items-center gap-4  md:gap-8 mb-8 md:mb-12 ${
+//         isReversed ? "md:flex-row-reverse" : ""
+//       }`}
+//     >
+//       <div className="flex flex-col relative w-full  md:w-auto order-2 md:order-none">
+//         <div className="relative mb-3">
+//           <h1 className="bg-white w-max py-2 px-4 text-lg font-medium shadow-sm max-sm:text-sm">
+//             {title}
+//           </h1>
+//           {/* <div className="w-0 h-0 border-l-[10px] border-l-transparent border-t-[15px] border-t-white border-r-[10px] border-r-transparent absolute bottom-[-15px] left-0 transform"></div> */}
+//         </div>
+// <span className="bg-gradient-to-t text-7xl md:text-8xl font-bold from-[#ffffff] to-[#FF7B07] text-transparent bg-clip-text order-1 md:order-none">
+//   {number}
+// </span>
+//     </div>
+//   );
+// };
 
 const HowItWork = () => {
-  const steps = [
-    { number: "01", title: "Enroll in Your Preferred Test Series" },
-    { number: "02", title: "Get Instant Access to Tests & Study Material" },
-    { number: "03", title: "Attempt Tests Online or Download PDFs" },
-    { number: "04", title: "Receive Answer Keys & Explanations" },
-    { number: "05", title: "Get Faculty Feedback & Performance Reports" },
-  ];
-
   return (
-    <div className="container mx-auto px-4 py-12 md:py-16">
-      <div className="text-center">
+    <div className="padding-bottom ">
+      <div className="flex justify-center">
         <CommonHeading2 title="How It Works?" />
       </div>
-      <div className="max-w-5xl mx-auto">
-        {steps.map((step, index) => (
-          <StepItem
-            key={step.number}
-            number={step.number}
-            title={step.title}
-            isReversed={index % 2 !== 0}
-          />
-        ))}
+      <div className="space-y-7 mt-20">
+        <div>
+          <div className="flex items-center">
+            <div className="w-[20%] h-[2px] bg-gradient-to-t from-[#ffffff] to-[#FF7B07]"></div>
+            <div className="bg-gradient-to-t w-full relative text-7xl md:text-8xl font-bold from-[#ffffff] to-[#FF7B07] text-transparent bg-clip-text order-1 md:order-none before:content-[''] before:bg-red-500 before:w-16 before:h-[2px]">
+              <div className="text-sm absolute bg-white text-black -top-14 left-10 ">
+                {/* Triangle */}
+                <div
+                  className="absolute left-0 top-full w-0 h-0 
+                    border-t-[20px] border-t-[#D6863A] 
+                    border-r-[40px] border-r-transparent"
+                ></div>
+                <h3 className="py-2 font-medium text-lg px-4">
+                  Enroll in Your Preferred Test Series
+                </h3>
+              </div>
+              <p> 01</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center flex-row-reverse">
+            <div className="w-[20%] h-[2px] bg-gradient-to-t from-[#ffffff] to-[#FF7B07]"></div>
+            <div className="bg-gradient-to-t w-full relative text-7xl md:text-8xl font-bold from-[#ffffff] to-[#FF7B07] text-transparent bg-clip-text">
+              <div className="text-sm absolute bg-white text-black -top-14 right-10 ">
+                {/* Triangle */}
+                <div
+                  className="absolute right-0 top-full w-0 h-0 
+                    border-t-[20px] border-t-[#D6863A] 
+                    border-l-[40px] border-l-transparent"
+                ></div>
+                <h3 className="py-2 font-medium text-lg px-4 text-orange">
+                  Get Instant Access to Tests & Study Material
+                </h3>
+              </div>
+              <p className="text-right"> 02</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center">
+            <div className="w-[20%] h-[2px] bg-gradient-to-t from-[#ffffff] to-[#FF7B07]"></div>
+            <div className="bg-gradient-to-t w-full relative text-7xl md:text-8xl font-bold from-[#ffffff] to-[#FF7B07] text-transparent bg-clip-text order-1 md:order-none before:content-[''] before:bg-red-500 before:w-16 before:h-[2px]">
+              <div className="text-sm absolute bg-white text-black -top-14 left-10 ">
+                {/* Triangle */}
+                <div
+                  className="absolute left-0 top-full w-0 h-0 
+                    border-t-[20px] border-t-[#D6863A] 
+                    border-r-[40px] border-r-transparent"
+                ></div>
+                <h3 className="py-2 font-medium text-lg px-4">
+                  Enroll in Your Preferred Test Series
+                </h3>
+              </div>
+              <p> 03</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center flex-row-reverse">
+            <div className="w-[20%] h-[2px] bg-gradient-to-t from-[#ffffff] to-[#FF7B07]"></div>
+            <div className="bg-gradient-to-t w-full relative text-7xl md:text-8xl font-bold from-[#ffffff] to-[#FF7B07] text-transparent bg-clip-text">
+              <div className="text-sm absolute bg-white text-black -top-14 right-10 ">
+                {/* Triangle */}
+                <div
+                  className="absolute right-0 top-full w-0 h-0 
+                    border-t-[20px] border-t-[#D6863A] 
+                    border-l-[40px] border-l-transparent"
+                ></div>
+                <h3 className="py-2 font-medium text-lg px-4 text-orange">
+                  Get Instant Access to Tests & Study Material
+                </h3>
+              </div>
+              <p className="text-right"> 04</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center">
+            <div className="w-[20%] h-[2px] bg-gradient-to-t from-[#ffffff] to-[#FF7B07]"></div>
+            <div className="bg-gradient-to-t w-full relative text-7xl md:text-8xl font-bold from-[#ffffff] to-[#FF7B07] text-transparent bg-clip-text order-1 md:order-none before:content-[''] before:bg-red-500 before:w-16 before:h-[2px]">
+              <div className="text-sm absolute bg-white text-black -top-14 left-10 ">
+                {/* Triangle */}
+                <div
+                  className="absolute left-0 top-full w-0 h-0 
+                    border-t-[20px] border-t-[#D6863A] 
+                    border-r-[40px] border-r-transparent"
+                ></div>
+                <h3 className="py-2 font-medium text-lg px-4">
+                  Enroll in Your Preferred Test Series
+                </h3>
+              </div>
+              <p> 05</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
